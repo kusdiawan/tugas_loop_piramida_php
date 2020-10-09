80a619095c7e577164bae9fa108126f2b4599d2e
kusdiawan <71364605+kusdiawan@users.noreply.github.com>
2020-10-09 09:52:45 +0700

    Update tugas_belahketupat.php

--- tugas_belahketupat.php
+++ tugas_belahketupat.php
@@ -1,19 +1,11 @@
 <?php
-for( $a=9; $a>0; $a--){
+for( $a=10; $a>0; $a--){
     for($i=1; $i<=$a; $i++){
-        echo "__";
+        echo "_";
     }for($a1=10; $a1>$a; $a1--){
-        echo"**";
-    }for($a2=9; $a2>$a; $a2--){
-        echo"**";
-    }echo"<br>";
-}for($b=0; $b<=9; $b++){
-    for($j=1; $j<=$b; $j++){
-        echo"__";
-    }for($b1=10; $b1>$b; $b1--){
-        echo"**";
-    }for($b2=9; $b2>$b; $b2--){
-        echo"**";
+        echo"*";
+    }for($a2=11; $a2>$a; $a2--){
+        echo"*";
     }echo"<br>";
 }
 ?>
